86f6807b618cced99f81eddfff6736bdd49a5ca2
Archibold Bernard <e.archibold@webforgegh.com>
2020-07-16 11:27:40 +0000

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Helmet} from "react-helmet";
+import ReactPlayer from "react-player"
+
+
 
 function App() {
+
+    const [verified, setVerified] = useState(true);
+    const [phone, setPhone] = useState("");
+    const [pin,setPin] = useState("");
+    const [loading, setLoading] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Helmet>
+        <title>Rancard Video Platform</title>
+      </Helmet>
+
+        <div className={"app-header"}>
+            <h4>Rancard Video Platform -  Demo</h4>
+        </div>
+
+        <div className={"app-body"}>
+            <div className={"app-video-container"}>
+                <div>
+                    <ReactPlayer
+                        controls={true}
+                        url="https://vimeo.com/3155182"
+                    />
+
+                    <div className={"video-details"}>
+                        <h2>Test Scating</h2>
+                        <p>This is a small description about the video</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className={"more-content-container"}>
+                More Content
+            </div>
+
+        </div>
+
+
+        <div className={`modal-verify ${verified ? "modal-hide" : "modal-show"}`}>
+
+            <div className={"modal-container"}>
+                <div >
+                    {
+                        loading && <div>Loading please wait....</div>
+                    }
+                </div>
+                <input type="phone" placeholder={"Phone number"} name={"msisdn"} className={"modal-input"}/>
+                <input type="number" placeholder={"Pin"} name={"pin"} className={"modal-input"}/>
+
+                <button onClick={()=>{
+                    setLoading(true);
+                    setTimeout(()=>{
+                        setLoading(false);
+                        setVerified(true);
+                    }, 2000)
+                }} disabled={loading} className={"btn btn-verify"}>Verify</button>
+                <button onClick={()=>{
+                    setLoading(true);
+                    setTimeout(()=>{
+                        setLoading(false);
+                        setVerified(true)
+                    }, 2000)
+                }} disabled={loading} className={"btn btn-subscribe"}>Subscribe</button>
+            </div>
+
+        </div>
+
     </div>
   );
 }
